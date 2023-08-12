d98159549a07422b98dbe99ca9feedf5dfa3be06
Alhanouf <hhsj200512@gmail.com>
2023-08-13 01:05:13 +0300

    ref

---
+++ main.js
@@ -0,0 +1,80 @@
+///Day 1///
+//الطباعة//
+console.log("Hello , world");
+console.log("Alhanouf Aloraini");
+
+///Day 2///
+//المتغيرات//
+//1-explicit//
+let username = "Alhanouf"
+
+//2-implicit//
+let My$Age;
+My_Age = "17"
+
+//var//
+var usernam$e= "Alhanouf"
+var username2="Alhanouf"
+
+//const//
+//We cant use (implicit)//
+const id = 777888999
+console.log(id);
+
+ //Day 3 //
+ ////Data types////
+ ///1-bool(Boolean)///
+ let bool= false;
+ console.log(bool);
+ console.log(typeof bool);
+
+ //2-null//
+ let n = null;
+ console.log(null);
+ console.log(typeof null);
+
+ 
+
+//3-symbol//
+const id$ =symbol(777888999);
+console.log(id);
+console.log(typeof id);
+
+
+//4-undefined//
+let un =undefined
+console.log(un);
+console.log(typeof un);
+
+//5- num (intger)//
+let int = 17
+console.log(int);
+console.log(typeof int);
+
+
+//6- num (double)//
+let dou =3.5
+console.log(dou);
+console.log(typeof dou);
+//7- num (Bigint)//
+let big = 131514n
+console.log(big);
+console.log(typeof big);
+
+//8-string//
+let str = "web design"
+console.log(str);
+console.log(typeof str);
+
+//9- string with num//
+let num1= "30"+"70"
+let num2= "70"-"30"
+console.log(num1);
+console.log(typeof num1);
+console.log(num2);
+console.log(typeof num2);
+
+//10- iterpolation//
+let coun1="Riyadh"
+let coun2="Brazil"
+console.log(`${coun1}located in ${coun2}`);
